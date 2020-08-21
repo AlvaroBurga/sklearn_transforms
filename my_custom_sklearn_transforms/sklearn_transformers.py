17967afce71e33c94aa8c0fa295bdc6b26c6b944
AlvaroBurga <51216883+AlvaroBurga@users.noreply.github.com>
2020-08-22 00:14:16 +0200

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,25 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+    
+# All sklearn Transforms must have the `transform` and `fit` methods
+class Normalizate():
+    def __init__(self):
+        self.columns = "a"
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        scaler = MinMaxScaler()
+        data = X.copy()
+        xfeatures = data.iloc[:,:-1]
+        ar_Norm = preprocessing.MinMaxScaler(xfeatures)
+        scaler.fit(xfeatures)
+        ar_Norm=scaler.transform(xfeatures)
+        df_Norm= pd.DataFrame(ar_Norm, columns=df_data_3.columns[:-1])
+        df_data_4 = pd.concat([df_Norm, df_data_3.iloc[:,-1:]], axis=1)
+        # Devolvemos el data frame con los datos normalizados
+        return df_data_4
