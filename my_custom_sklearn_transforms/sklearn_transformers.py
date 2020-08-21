08de29cec24126b0e3c49d7394e68ee962e524cd
AlvaroBurga <51216883+AlvaroBurga@users.noreply.github.com>
2020-08-22 00:49:29 +0200

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -15,7 +15,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
     
-    
+from sklearn.preprocessing import MinMaxScaler   
 # All sklearn Transforms must have the `transform` and `fit` methods
 class Normalizate():
     def __init__(self):
